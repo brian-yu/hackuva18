04b94e574e72d86c21871d8cfa0bcbfe9c236193
Brian Yu <brian@byu.io>
2018-03-24 17:21:06 -0400

    topic actions

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -84,90 +84,11 @@ function shouldFetchTopic(state, id) {
 	}
 }
 
-export function fetchTopicIfNeeded(stateAbbrev) { 
+export function fetchTopicIfNeeded(id) { 
   return (dispatch, getTopic) => {
-    if (shouldFetchTopic(getTopic(), stateAbbrev)) {
+    if (shouldFetchTopic(getTopic(), id)) {
       // Dispatch a thunk from thunk!
-      return dispatch(fetchTopic(stateAbbrev))
-    } else {
-      // Let the calling code know there's nothing to wait for.
-      return Promise.resolve()
-    }
-  }
-}
-
-/*
-* Async request/receieve/invalidate for congressman info
-*/
-
-export const REQUEST_CONGRESSMAN = 'REQUEST_CONGRESSMAN';
-export const RECEIVE_CONGRESSMAN = 'RECEIVE_CONGRESSMAN';
-export const INVALIDATE_CONGRESSMAN = 'INVALIDATE_CONGRESSMAN';
-
-export function invalidateCongressman(bid) {
-  return {
-    type: INVALIDATE_CONGRESSMAN,
-    bid
-  }
-}
-
-
-export function requestCongressman(bid) {
-  return {
-      type: REQUEST_CONGRESSMAN,
-      bid
-    }
-}
-
-export function receiveCongressman(bid, json) {
-  console.log(json);
-  return {
-    type: RECEIVE_CONGRESSMAN,
-    bid,
-    data: json,
-    receivedAt: Date.now()
-  }
-}
-
-export function fetchCongressman(bid) {
-  // Thunk middleware knows how to handle functions.
-  // It passes the dispatch method as an argument to the function,
-    // thus making it able to dispatch actions itself.
- 
-  return function (dispatch) {
-    // First dispatch: the app state is updated to inform
-    // that the API call is starting.
-   
-    dispatch(requestCongressman(bid))
-   
-    // The function called by the thunk middleware can return a value,
-    // that is passed on as the return value of the dispatch method.
-     
-    // In this case, we return a promise to wait for.
-    // This is not required by thunk middleware, but it is convenient for us.
-
-    return fetch(`/api/congressman/${bid}`)
-      .then(response => response.json())
-      .then(json => dispatch(receiveCongressman(bid, json)))
-  }
-}
-
-function shouldFetchCongressman(state, bid) {
-  const data = state.dataByCongressman[bid]
-  if (!data) {
-    return true
-  } else if (data.isFetching) {
-    return false
-  } else {
-    return data.didInvalidate
-  }
-}
-
-export function fetchCongressmanIfNeeded(bid) { 
-  return (dispatch, getTopic) => {
-    if (shouldFetchCongressman(getTopic(), bid)) {
-      // Dispatch a thunk from thunk!
-      return dispatch(fetchCongressman(bid))
+      return dispatch(fetchTopic(id))
     } else {
       // Let the calling code know there's nothing to wait for.
       return Promise.resolve()
