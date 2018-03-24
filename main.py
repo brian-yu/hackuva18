7174bbbc527cc9625f888ccf46124ac6de2b8921
Brian Yu <brian@byu.io>
2018-03-24 15:10:15 -0400

    add flask

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, url_for, jsonify, send_from_directory
+import requests, os, xmltodict, json, re, pickle
+from collections import OrderedDict
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+from random import randint
+
+############################################### CONFIG ###############################################
+
+# app = Flask(__name__)
+app = Flask(__name__, static_folder='app/build/')
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+############################################### ROUTES ###############################################
+
