0007f10229bfbecb3e0650e6320143c37150a40e
Brian Yu <brian@byu.io>
2018-03-25 10:54:54 -0400

    Companies

--- app/src/components/Topic.js
+++ app/src/components/Topic.js
@@ -37,7 +37,7 @@ class Topic extends Component {
 	CompanyItem = (props) => {
 		return (
   		<li>
-  			<img src={props.image}/>
+  			<img src={props.image} alt={props.name}/>
   			<h5>{props.name}</h5>
   		</li>
   	);
@@ -61,30 +61,36 @@ class Topic extends Component {
 	RelatedList = (props) => {
 		const related = props.data.resources.related;
 		var i = 0;
-	  return (
-	    <ul>
-	      {related.map((topic) =>
-	        <this.ListLink key={i++}
-	                  text={topic.text}
-	                  id={topic.id}/>
+	  return related.length === 0 ? null : (
+	  	<div id="related">
+	  		<h3>Related</h3>
+		    <ul>
+		      {related.map((topic) =>
+		        <this.ListLink key={i++}
+		                  text={topic.text}
+		                  id={topic.id}/>
 
-	      )}
-	    </ul>
+		      )}
+		    </ul>
+	    </div>
 	  );
 	}
 
 	CompanyList = (props) => {
 		const companies = props.data.companies;
 		var i = 0;
-	  return (
-	    <ul>
-	      {companies.map((company) =>
-	        <this.CompanyItem key={i++}
-	                  image={company.image}
-	                  name={company.name}/>
+	  return companies === "" ? null : (
+	  	<div id="companies">
+	  		<h3>Companies</h3>
+		    <ul>
+		      {companies.map((company) =>
+		        <this.CompanyItem key={i++}
+		                  image={company.image}
+		                  name={company.name}/>
 
-	      )}
-	    </ul>
+		      )}
+		    </ul>
+	    </div>
 	  );
 	}
 
@@ -101,15 +107,8 @@ class Topic extends Component {
 	    			<h3>Resources</h3>
 	    			<this.ResourceList data={this.props.dataByTopic[this.props.topic].data}/>
 	    		</div>
-	    		<div>
-	    			<h3>Related</h3>
-	    			<this.RelatedList data={this.props.dataByTopic[this.props.topic].data}/>
-	    		</div>
-	    		<div id="companies">
-	    			<h3>Companies</h3>
-	    			<this.CompanyList data={this.props.dataByTopic[this.props.topic].data}/>
-	    		</div>
-
+	    		<this.RelatedList data={this.props.dataByTopic[this.props.topic].data}/>
+	    		<this.CompanyList data={this.props.dataByTopic[this.props.topic].data}/>
 	    	</div>
     	)
   	}
